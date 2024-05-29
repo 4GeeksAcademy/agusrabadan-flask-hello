55e768b76062d9564bccf7f3eb8c3679ce26e420
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-05-29 19:38:28 +0000

    sigue

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, Users
+from .models import db, Users, Posts
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,4 +10,5 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Posts, db.session))
+    #admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -3,16 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Users(db.Model):
+    #Atributos
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column (db.String(), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email}>'
 
     def serialize(self):
         # do not serialize the password, its a security breach
         return {"id": self.id,
                 "email": self.email,
-                "is_active": self.is_active}
+                "is_active": self.is_active,
+                "first_name": self.first_name,
+                "last_name": self.last_name}
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_to = db.relationship("Users", foreign_keys=[user_id])
+
+    def __repr__(self):
+        return f'<Post: {self.title}>'
+
+    def serialize(self):
+        # do not serialize the password, its a security breach
+        return {"id": self.id,
+                "title": self.title,
+                "user_id": self.user_id
+                }
