07f37244b4c33b47c5326fbefdab59dbfd4205e2
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-05-31 11:46:42 +0000

    add posts and comments

--- src/api/models.py
+++ src/api/models.py
@@ -27,7 +27,12 @@ class Posts(db.Model):
     title = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user_to = db.relationship("Users", foreign_keys=[user_id])
+    description = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    publication_date = db.Column(db.Date)
+    image_url = db.Column (db.String)
 
+    
     def __repr__(self):
         return f'<Post: {self.title}>'
 
@@ -35,5 +40,35 @@ class Posts(db.Model):
         # do not serialize the password, its a security breach
         return {"id": self.id,
                 "title": self.title,
-                "user_id": self.user_id
+                "user_id": self.user_id,
+                "description": self.description,
+                "body": self.body,
+                "publication_date": self.publication_date,
+                "image_url": self.image_url
                 }
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    comment_date = db.Column(db.Date)
+    post_id = db.Column (db.Integer, db.ForeignKey("posts.id"))
+    comment_to = db.relationship("Posts", foreign_keys=[post_id])
+    author_id = db.Column (db.Integer, db.ForeignKey("users.id"))
+    author = db.relationship("Users", foreign_keys=[author_id])
+
+    
+    def __repr__(self):
+        return f'<Comment: {self.title}>'
+
+    def serialize(self):
+        # do not serialize the password, its a security breach
+        return {"id": self.id,
+                "body": self.body,
+                "comment_date": self.comment_date,
+                "post_id": self.post_id,
+                "author_id": self.author_id
+                
+                }
+
+
+
